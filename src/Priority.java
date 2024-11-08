8f051fe18c4698290434d15b0b04b0de0aaa65b7
Jacob Atterson <atterjac000@warren.k12.in.us>
2024-11-08 13:49:26 -0500

    Initial commit

---
+++ src/Priority.java
@@ -0,0 +1,16 @@
+public enum Priority {
+    LOW("Low urgency"),
+    MEDIUM("Medium urgency"),
+    HIGH("High urgency");
+
+    private final String description;
+
+    Priority(String description) {
+        this.description = description;
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
